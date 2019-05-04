77edd8ee92bc84bce11ef253fe08f7b07160e922
Carl Gross <10165488+cagross@users.noreply.github.com>
2019-05-04 17:15:42 +0700

    Lock down site.

--- wp-content/themes/adventure-tours-child/functions.php
+++ wp-content/themes/adventure-tours-child/functions.php
@@ -12,3 +12,26 @@ function filter_adventure_tours_get_theme_styles( $default_set ) {
 	return $default_set;
 }
 add_filter( 'get-theme-styles', 'filter_adventure_tours_get_theme_styles' );
+
+// This locks down my entire site to non-registered users.
+function redirect_func() {
+ if( ! is_user_logged_in() && !( $GLOBALS['pagenow'] === 'wp-login.php') ) { if ( ! is_public_page_post() ) { auth_redirect(); } }
+}
+// This opens up a page/post of my choice to the public.  Mark page with a custom field of "show" and a value of 1.
+function is_public_page_post() {
+    if ( ! ( is_single() || is_page () ) ) : return false; endif;// If you want to open up the blog page, comment out this line.
+    $id = get_the_ID();
+    $hide = get_post_meta ($id, 'show', true);
+    if ( $hide == 1 ): return true; endif;
+    return false;
+}
+add_action( 'template_redirect', 'redirect_func' );
+
+// This function displays the name of the template used at the bottom of the page.
+function show_template() {
+    if( is_super_admin() ){
+        global $template;
+        print_r($template);
+    }
+}
+add_action('wp_footer', 'show_template');
